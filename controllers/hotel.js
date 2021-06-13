48b3b2d7b469d5436ab2c66420baa2164c81b714
Shakil Ahmed Atik <shakilatik@gmail.com>
2021-06-13 10:59:38 +0600

    Final Commit

--- controllers/hotel.js
+++ controllers/hotel.js
@@ -1,4 +1,5 @@
 import Hotel from '../models/hotel'
+import Order from '../models/order'
 import fs from 'fs'
 
 export const create = async (req, res) => {
@@ -32,6 +33,7 @@ export const create = async (req, res) => {
 }
 
 export const hotels = async (req, res) => {
+  // let all = await Hotel.find({ from: { $gte: new Date() } })
   let all = await Hotel.find({})
     .limit(24)
     .select('-image.data')
@@ -67,8 +69,86 @@ export const remove = async (req, res) => {
 
 export const read = async (req, res) => {
   let hotel = await Hotel.findById(req.params.hotelId)
+    .populate('postedBy', '_id name')
     .select('-image.data')
     .exec()
-  console.log('SINGLE HOTEL', hotel)
+  // console.log("SINGLE HOTEL", hotel);
   res.json(hotel)
 }
+
+export const update = async (req, res) => {
+  try {
+    let fields = req.fields
+    let files = req.files
+
+    let data = { ...fields }
+
+    if (files.image) {
+      let image = {}
+      image.data = fs.readFileSync(files.image.path)
+      image.contentType = files.image.type
+
+      data.image = image
+    }
+
+    let updated = await Hotel.findByIdAndUpdate(req.params.hotelId, data, {
+      new: true,
+    }).select('-image.data')
+
+    res.json(updated)
+  } catch (err) {
+    console.log(err)
+    res.status(400).send('Hotel update failed. Try again.')
+  }
+}
+
+export const userHotelBookings = async (req, res) => {
+  const all = await Order.find({ orderedBy: req.user._id })
+    .select('session')
+    .populate('hotel', '-image.data')
+    .populate('orderedBy', '_id name')
+    .exec()
+  res.json(all)
+}
+
+export const isAlreadyBooked = async (req, res) => {
+  const { hotelId } = req.params
+  // find orders of the currently logged in user
+  const userOrders = await Order.find({ orderedBy: req.user._id })
+    .select('hotel')
+    .exec()
+  // check if hotel id is found in userOrders array
+  let ids = []
+  for (let i = 0; i < userOrders.length; i++) {
+    ids.push(userOrders[i].hotel.toString())
+  }
+  res.json({
+    ok: ids.includes(hotelId),
+  })
+}
+
+export const searchListings = async (req, res) => {
+  const { location, date, bed } = req.body
+  // console.log(location, date, bed);
+  // console.log(date);
+  const fromDate = date.split(',')
+  // console.log(fromDate[0]);
+  let result = await Hotel.find({
+    from: { $gte: new Date(fromDate[0]) },
+    location,
+  })
+    .select('-image.data')
+    .exec()
+  // console.log("SEARCH LISTINGS", result);
+  res.json(result)
+}
+
+/**
+ * if you want to be more specific
+ let result = await Listing.find({
+  from: { $gte: new Date() },
+  to: { $lte: to },
+  location,
+  bed,
+})
+ */
--- routes/hotel.js
+++ routes/hotel.js
@@ -13,6 +13,10 @@ import {
   sellerHotels,
   remove,
   read,
+  update,
+  userHotelBookings,
+  isAlreadyBooked,
+  searchListings,
 } from '../controllers/hotel'
 
 router.post('/create-hotel', requireSignin, formidable(), create)
@@ -21,5 +25,16 @@ router.get('/hotel/image/:hotelId', image)
 router.get('/seller-hotels', requireSignin, sellerHotels)
 router.delete('/delete-hotel/:hotelId', requireSignin, hotelOwner, remove)
 router.get('/hotel/:hotelId', read)
+router.put(
+  '/update-hotel/:hotelId',
+  requireSignin,
+  hotelOwner,
+  formidable(),
+  update
+)
+// orders
+router.get('/user-hotel-bookings', requireSignin, userHotelBookings)
+router.get('/is-already-booked/:hotelId', requireSignin, isAlreadyBooked)
+router.post('/search-listings', searchListings)
 
 module.exports = router
